f634e5cd19120beee35a92d2874ce2c05affc28b
ed <github@whatsa.co.uk>
2017-09-03 20:05:34 +0100

    Fix build - add missing brace

--- com.acme.prime.api/src/com/acme/prime/eval/api/Eval.java
+++ com.acme.prime.api/src/com/acme/prime/eval/api/Eval.java
@@ -6,7 +6,7 @@ import org.osgi.annotation.versioning.ProviderType;
  * A service that evaluates an expression and returns the result
  */
 @ProviderType
-public interface Eval 
+public interface Eval {
 	/**
 	 * Evaluate an expression and return the result.
 	 */
